1d905dd1a7ba606eb65203be5ec98318a18ff54b
AemonYwj <905812177@qq.com>
2022-07-20 22:57:42 +0800

    完成了作业一

--- pyHw1.py
+++ pyHw1.py
@@ -1,6 +1,19 @@
-import numpy as np
+# -*- coding: UTF-8 -*-
 
-def testfun(x)
+def testfun(x):
     return x
 
-t = int(input("test"))
+income = float(input("2022年月税后收入: "))
+consumpt = float(input("2021年月消费总额: "))
+#deposit = float(input("2021年月储蓄总额"))
+profit = float(input("2021年月投资收入: "))
+target = float(input("所需房屋首付款: "))
+
+deposit = income+profit-consumpt
+
+time = round(target/deposit) 
+
+month = time % 12
+year = int((time-month)/12)
+
+print("需要",year,"年",month,"月")
